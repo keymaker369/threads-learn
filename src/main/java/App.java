9a14cf5b56dbe61a42022dda8bf6d1becc0eeb11
Nenad Seke <nseke@theatermania.com>
2014-04-23 16:29:43 -0400

    init

---
+++ src/main/java/App.java
@@ -0,0 +1,30 @@
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+
+
+public class App {
+
+	public static void main(String... args) {
+
+		ExecutorService executorService =
+				  new ThreadPoolExecutor(
+				    15, // core thread pool size
+				    20, // maximum thread pool size
+				    1, // time to wait before resizing pool
+				    TimeUnit.SECONDS,
+				    new ArrayBlockingQueue<Runnable>(20, true),
+				    new ThreadPoolExecutor.CallerRunsPolicy());
+
+
+		for (int i = 1; i < 12; i++) {
+			MyThread myThread = new MyThread("Moj tred " + i);
+//			myThread.setDaemon(true);
+			myThread.start();
+//			executorService.execute(myThread);
+		}
+		System.out.println("kraj");
+	}
+}
---
+++ src/main/java/MyThread.java
@@ -0,0 +1,30 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
+
+public class MyThread extends Thread {
+
+	public MyThread() {
+	}
+
+	public MyThread(String name) {
+		super(name);
+	}
+
+	@Override
+	public void run() {
+		int min = 0;
+		int max = 10;
+		int secondsWait = (new Random().nextInt((max - min) + 1) + min) * 1000;
+
+		System.out.println("Start: " + new SimpleDateFormat("EEE, yyyy-MM-dd HH:mm:ss.SSSXXX").format(new Date(System.currentTimeMillis())) + this.getName() + " wait: " + secondsWait);
+
+		try {
+			sleep(secondsWait);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("End: " + new SimpleDateFormat("EEE, yyyy-MM-dd HH:mm:ss.SSSXXX").format(new Date(System.currentTimeMillis())) + this.getName() + " wait: " + secondsWait);
+	}
+}
